8edd6c03352bcc21c2d4d04a97a6855f1942d655
Olesia Cheremnykh <chereole@fel.cvut.cz>
2023-03-19 14:55:24 +0100

    download db file

--- src/components/DbFileUpload.jsx
+++ src/components/DbFileUpload.jsx
@@ -1,10 +1,11 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {DbContext, SqlContext} from "./context/context";
 
 const DbFileUpload = () => {
 
-    const {setDb} = useContext(DbContext);
+    const {db, setDb} = useContext(DbContext);
     const {sql} = useContext(SqlContext);
+    const [setDataArray] = useState(null);
 
     function handleFile(e) {
         const inputFile = e.target.files[0];
@@ -12,17 +13,33 @@ const DbFileUpload = () => {
         reader.onload = function () {
             const buffer = this.result;
             const array = new Uint8Array(buffer);
+            setDataArray(array);
             const db = new sql.Database(array);
             setDb(db);
         };
         reader.readAsArrayBuffer(inputFile);
     }
 
+    function downloadFile(e) {
+        e.preventDefault();
+        const blob = new Blob([db.export()]);
+        const a = document.createElement("a");
+        document.body.appendChild(a);
+        a.href = window.URL.createObjectURL(blob);
+        a.download = "sql.db";
+        a.onclick = function () {
+            setTimeout(function () {
+                window.URL.revokeObjectURL(a.href);
+            }, 1500);
+        };
+        a.click();
+    }
+
     return (
         <div>
             <label className="button">Load an SQLite database file: </label>
-            <input type='file' id='dbfile' onChange={(event => handleFile(event))}/>
-            <button>Upload</button>
+            <input type='file' id='dbfile' onChange={event => handleFile(event)}/>
+            <button onClick={event => downloadFile(event)}>Download</button>
         </div>
     );
 };

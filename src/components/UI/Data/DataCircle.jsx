6896a09dd362f9b8efbf5b45892ba7bb2486c3e6
Olesia Cheremnykh <chereole@fel.cvut.cz>
2023-03-20 22:38:46 +0100

    collisions check 😎

--- src/components/UI/Data/DataCircle.jsx
+++ src/components/UI/Data/DataCircle.jsx
@@ -1,21 +1,9 @@
-import React, {useLayoutEffect, useRef, useState} from 'react';
 import style from "./DataCircle.module.css"
 
-const DataCircle = ({ text }) => {
-    const circleRef = useRef(null);
-    const [position, setPosition] = useState({ top: 0, left: 0 });
-
-    useLayoutEffect(() => {
-        const container = document.getElementById("circle-container");
-        const circleRect = circleRef.current.getBoundingClientRect();
-        const newTop = Math.random() * (container.offsetHeight - circleRect.height);
-        const newLeft = Math.random() * (container.offsetWidth - circleRect.width);
-        setPosition({ top: newTop, left: newLeft });
-    }, []);
-
+const DataCircle = ({ text, top, left }) => {
     return (
-        <div ref={circleRef} id={`circle-${text}`}
-             className={style.circle} style={{ ...position}}>
+        <div id={`circle-${text}`} className={style.circle}
+             style={{ top, left}}>
             <p>{text}</p>
         </div>
     );
--- src/components/UI/Data/DataCircleContainer.jsx
+++ src/components/UI/Data/DataCircleContainer.jsx
@@ -17,10 +17,54 @@ const DataCircleContainer = () => {
         }
     }
 
+    const getPositions = () => {
+        return Math.random() * (300 - 20);
+    }
+
+    const findCollidingCircles = (circles) => {
+        let collisionObjects = [];
+        for (let i = 0; i < circles.length; i++) {
+            for (let j = i + 1; j < circles.length; j++) {
+                if (circles[i].props.left < circles[j].props.left + 30 &&
+                    circles[i].props.left + 30 > circles[j].props.left &&
+                    300 - circles[i].props.top < 300 - circles[j].props.top + 30 &&
+                    300 - circles[i].props.top + 30 > 300 - circles[j].props.top) {
+                    collisionObjects.push(circles[i])
+                }
+            }
+        }
+        return collisionObjects;
+    }
+
+    const checkCollisions = (allCircles) => {
+        const collisionObjects = findCollidingCircles(allCircles);
+
+        if (collisionObjects.length === 0) {
+            setCircles(allCircles);
+            return;
+        }
+
+        const newCircles = [...allCircles];
+        for (let object of collisionObjects) {
+            let newLeft = getPositions(200);
+            let newTop = getPositions(200);
+            const index = newCircles.findIndex(circle => circle.key === object.key);
+            newCircles[index] =
+                <DataCircle key={object.key} text={"a"} left={newLeft} top={newTop}/>;
+        }
+
+        checkCollisions(newCircles);
+    }
+
+
     useEffect(() => {
         const values = getAllFromSelectedTable();
-        const newCircles = values.map(item => <DataCircle key={item[0]} text={item[0]} />);
-        setCircles(newCircles);
+        let newCircles = values.map(
+            item => <DataCircle
+                key={item[0]} text={item[0]}
+                left={getPositions(200)} top={getPositions(200)}/>
+        );
+        checkCollisions(newCircles)
     }, [db]);
 
     if (error) return <div> Error: {error} </div>;

fe65734f7d6d5dd142653aa7eec6d71d48622e83
Olesia Cheremnykh <chereole@fel.cvut.cz>
2023-04-02 23:10:59 +0200

    better diagram size

--- src/components/nodes/DatabaseDiagram.jsx
+++ src/components/nodes/DatabaseDiagram.jsx
@@ -26,117 +26,23 @@ const defaultViewport = {x: 0, y: 0, zoom: 1.5};
 const nodeTypes = {attributeNode: AttributeNode};
 
 
-const nodess = [
-    {
-        id: 'A',
-        data: {label: 'Parent node xui'},
-        position: {x: 0, y: 0},
-        style: {
-            width: 170,
-            height: 140,
-        }
-    },
-    {
-        id: 'B',
-        data: {label: 'child node 1'},
-        type: 'attributeNode',
-        draggable: false,
-        position: {x: 10, y: 30},
-        parentNode: 'A',
-        extent: 'parent',
-    },
-    {
-        id: 'C',
-        data: {label: 'child node 1'},
-        type: 'attributeNode',
-        draggable: false,
-        position: {x: 10, y: 52},
-        parentNode: 'A',
-        extent: 'parent',
-    },
-    {
-        id: 'D',
-        data: {label: 'child node 1'},
-        type: 'attributeNode',
-        draggable: false,
-        position: {x: 10, y: 90},
-        parentNode: 'A',
-        extent: 'parent',
-    },
-    {
-        id: 'E',
-        type: 'attributeNode',
-        data: {label: 'child node 2'},
-        position: {x: 10, y: 120},
-        parentNode: 'A',
-        draggable: false,
-        extent: 'parent',
-    },
-
-
-    {
-        id: 'AA',
-        data: {label: 'Parent node xui'},
-        position: {x: 100, y: 100},
-        style: {
-            width: 170,
-            height: 140,
-        }
-    },
-    {
-        id: 'BA',
-        data: {label: 'child node 1'},
-        type: 'attributeNode',
-        draggable: false,
-        position: {x: 10, y: 30},
-        parentNode: 'AA',
-        extent: 'parent',
-    },
-    {
-        id: 'CA',
-        data: {label: 'child node 1'},
-        type: 'attributeNode',
-        draggable: false,
-        position: {x: 10, y: 52},
-        parentNode: 'AA',
-        extent: 'parent',
-    },
-    {
-        id: 'DA',
-        data: {label: 'child node 1'},
-        type: 'attributeNode',
-        draggable: false,
-        position: {x: 10, y: 90},
-        parentNode: 'AA',
-        extent: 'parent',
-    },
-    {
-        id: 'EA',
-        type: 'attributeNode',
-        data: {label: 'child node 2'},
-        position: {x: 10, y: 120},
-        parentNode: 'AA',
-        draggable: false,
-        extent: 'parent',
-    }
-];
 const DatabaseDiagram = () => {
     const {db} = useContext(DbContext);
-    const [nodes, setNodes] = useNodesState(nodess);
+
     const [edges, setEdges] = useEdgesState([]);
 
     function parseTables() {
         const res = db.exec(GET_ALL_TABLES_DATA_QRY)[0].values;
-        const attributeHeight = 30;
+        const attributeHeight = 25;
         let nodes = [];
         for (let table of res) {
             nodes.push({
                 id: table[0],
-                data: {label: 'Parent node xui'},
+                data: {label: table[0]},
                 position: {x: 0, y: 0},
                 style: {
                     width: 170,
-                    height: table[1] * attributeHeight + 10,
+                    height: 30 + (table[1] * attributeHeight) ,
                 }
             })
             const attributes = table[2].split(',');
@@ -147,14 +53,17 @@ const DatabaseDiagram = () => {
                     data: {label: attributes[i]},
                     type: 'attributeNode',
                     draggable: false,
-                    position: {x: 10, y: 30 + (22 * i)},
+                    position: {x: 10, y: 30 + (25 * i)},
                     parentNode: table[0],
                     extent: 'parent',
                 })
             }
         }
+        return nodes;
     }
 
+    const nodess = parseTables();
+    const [nodes, setNodes] = useNodesState(nodess);
 
     const onNodesChange = useCallback(
         (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
@@ -185,7 +94,7 @@ const DatabaseDiagram = () => {
             fitView
             attributionPosition="bottom-left"
         >
-            <MiniMap/>
+            <MiniMap zoomable pannable/>
             <Controls/>
         </ReactFlow>
     );

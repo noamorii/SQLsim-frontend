a1216aaa452ff3ace455715cd854f30c23d029ba
Erkan <abajour@Erkans-MacBook-Pro.local>
2023-04-10 16:59:53 +0300

    chore: add build directory to .gitignore feat: add .nvmrc file with Node.js version 16 fix(craco.config.js): add fallback for path and fs modules feat(package.json): add path-browserify and react-scripts dependencies fix(SideMenu.jsx): import correct AiOutlineClose icon from react-icons/ai The .gitignore file now ignores the build directory. The .nvmrc file is added to specify the Node.js version 16. The craco.config.js file now has a fallback for the path and fs modules. The package.json file now has the path-browserify and react-scripts dependencies. The SideMenu.jsx file now imports the correct AiOutlineClose icon from react-icons/ai.

--- src/components/UI/SideMenu/SideMenu.jsx
+++ src/components/UI/SideMenu/SideMenu.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {Link} from "react-router-dom";
-import {AiOutlineClose} from "react-icons/all";
+import {AiOutlineClose} from "react-icons/ai";
 import './SideMenu.css';
 
 const SideMenu = () => {

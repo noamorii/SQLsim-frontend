3babe7074509aea2bfdf009421012e0a8a688621
erkanercan <erkanercandev@gmail.com>
2023-04-10 17:18:59 +0300

    build: add support for HashRouter and gh-pages deployment The HashRouter is added to support deployment to GitHub pages. The gh-pages package is added as a devDependency to enable deployment to GitHub pages.

--- craco.config.js
+++ craco.config.js
@@ -15,6 +15,9 @@ module.exports = {
       resolve: {
         fallback: {
           path: require.resolve("path-browserify"),
+          crypto: require.resolve("crypto-browserify"),
+          stream: require.resolve("stream-browserify"),
+          buffer: require.resolve("buffer/"),
           fs: false,
         },
       },

99700a023d7b20c641d00756015936bbbf31fd03
Olesia Cheremnykh <chereole@fel.cvut.cz>
2023-04-26 22:12:44 +0200

    from element changing

--- src/components/visualisation/selectQuery/SelectQueryForm.jsx
+++ src/components/visualisation/selectQuery/SelectQueryForm.jsx
@@ -2,16 +2,22 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import './SelectQueryForm.css';
 import OptionsMenu from "./OptionsMenu";
+import { useNavigate  } from 'react-router-dom';
 
 const SelectQueryForm = () => {
     const { register, handleSubmit } = useForm();
     const [queryError, setQueryError] = useState(null);
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/tables');
+    }
 
     const savedQuery = sessionStorage.getItem('savedFromQuery');
     const loadFromQuery = () => {
         const element = savedQuery ? JSON.parse(savedQuery).split(' ').slice(2).join(' ')
-                   : 'Please create from query';
-        return <div className='loaded element disabled'>{element}</div>
+                   : 'Click here to create FROM query';
+        return <div onClick={handleClick} className='loaded element disabled'>{element}</div>
     };
 
     const startElements = [
@@ -25,11 +31,18 @@ const SelectQueryForm = () => {
 
     return (
         <div className='selectQueryContainer'>
-            <form id='form' className='selectQueryForm' onSubmit={onSubmitFrom}>
-                {elements.map((element, index) => (
-                    <div key={index}>{element}</div>
-                ))}
-            </form>
+            <div className="formAndButtons">
+                <form id='form' className='selectQueryForm' onSubmit={onSubmitFrom}>
+                    {elements.map((element, index) => (
+                        <div key={index}>{element}</div>
+                    ))}
+                </form>
+                <div className="buttonPanel">
+                    <button>Hello</button>
+                    <button>Hello</button>
+                    <button>Hello</button>
+                </div>
+            </div>
             {queryError && <div className='query-error'> {queryError.toString()}.</div>}
             <div className='optionsMenu'>
                 <OptionsMenu/>

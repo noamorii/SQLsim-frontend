74ec681a743375324a0eb46e328b054437d51496
Olesia Cheremnykh <chereole@fel.cvut.cz>
2023-05-04 00:45:14 +0200

    button change

--- src/components/UI/Buttons/Button.jsx
+++ src/components/UI/Buttons/Button.jsx
@@ -15,8 +15,8 @@ import styles from "./Button.module.css";
  */
 const Button = ({text, onClick, className, disabled, icon}) => {
     return (
-        <div className={styles.btn}>
-            <button className={className} onClick={onClick} disabled={disabled}>
+        <div className={styles.btn} onClick={onClick}>
+            <button className={className} disabled={disabled}>
                 {text}
             </button>
             {icon}
